8c3a97ad1d0ef3425aed6a47734de994420416de
raymondly <rla65@sfu.ca>
2023-07-14 00:42:21 -0700

    add GCP SQL connector library

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,25 @@
-import { useAuthContext} from "../hooks/useAuthContext"
+import { useAuthContext } from "../hooks/useAuthContext";
 import useLogout from "../hooks/useLogout";
-
-
-
-  
 
 export default function Home() {
   const { user } = useAuthContext();
-
 
   const logout = useLogout();
 
   return (
     <>
-      <h1 className='text-blue-600 text-4xl font-display font-semibold italic'>Date Planner</h1>
-      
-      {user &&
+      <h1 className="text-blue-600 text-4xl font-display font-semibold italic">
+        Date Planner
+      </h1>
+
+      {user && (
         <>
           <div>Welcome back {user.username}!</div>
-          <button className="border-black border p-1" onClick={logout}>Logout</button>
+          <button className="border-black border p-1" onClick={logout}>
+            Logout
+          </button>
         </>
-      }
+      )}
     </>
-  )
+  );
 }
