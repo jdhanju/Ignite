ae5acf4b175c728010d0fe5bd20374d63f0fe23a
raymondly <rla65@sfu.ca>
2023-07-30 16:00:22 -0700

    Changed all localhost to env

--- server/initDB.js
+++ server/initDB.js
@@ -1,8 +1,19 @@
 import pg from "pg";
 import dotenv from "dotenv";
+import { Connector } from "@google-cloud/cloud-sql-connector"
 dotenv.config();
 
+// Google Cloud SQL connector
+const connector = new Connector();
+
+const clientOpts = await connector.getOptions({
+  instanceConnectionName: process.env.GCP_CLOUD_SQL_INSTANCE_CONNECTION_NAME,
+  ipType: 'PUBLIC', 
+});
+
+
 const client = new pg.Client({
+  ...clientOpts,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
 });
@@ -15,6 +26,7 @@ const createDatabase = async () => {
 
 const createTables = async () => {
   const pool = new pg.Pool({
+    ...clientOpts,
     user: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
     database: "dateplanner",
